1931ac80f45f2b6a91dc926141bcd4c32174a1ce
vitorpiresc <vitorpiresc@gmail.com>
2024-02-15 17:30:34 -0300

    Initial Commit

---
+++ ArtworksApp/ArtworksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtworksAppApp.swift
+//  ArtworksApp
+//
+//  Created by Vitor Pires on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ArtworksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtworksApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArtworksApp
+//
+//  Created by Vitor Pires on 15/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
